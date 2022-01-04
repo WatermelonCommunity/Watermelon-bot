2aba3449d85f652b1cd8b6affb68a913169297c5
BooleanTrue-js <tram24@gmail.com>
2022-01-03 22:32:01 -0400

    Overwriting to Typescript

--- src/events/ready.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'ready',
-    once: true,
-    execute(client){
-        client.user.setActivity('Bot en desarrollo.', { type: 'WATCHING' });
-        console.log(`Inicializado como ${client.user.username}`);
-    },
-};
