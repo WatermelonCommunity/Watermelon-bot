f7a4d7c7917c66d4888b0344e44b8aec7efe70c3
mrLuisFer <mrluisfeer@gmail.com>
2022-01-09 13:06:21 -0600

    fix type errors and details

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-import { Client, Collection, Intents } from 'discord.js';
+import { Client, Collection, CommandInteraction, Intents } from 'discord.js';
 import * as fs from 'fs';
 import token from './env.json';
 
@@ -6,26 +6,24 @@ const client: Client = new Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS]
 });
 
-//  @ts-ignore
+// @ts-ignore
 client.commands = new Collection();
 const commandFiles: string[] = fs.readdirSync('./src/commands').filter((file) => file.endsWith('.js'));
 const eventFiles: string[] = fs.readdirSync('./src/events').filter((file) => file.endsWith('.js'));
 
-for (const file of commandFiles) {
+commandFiles.forEach((file) => {
     const command = require(`./src/commands/${file}`);
-    //  @ts-ignore
-    client.commands.set(command.default.data.name, command);
-}
+    client?.application?.commands.set(command.default.data.name, command);
+});
 
-client.on('interactionCreate', async (interaction) => {
-    if (!interaction.isCommand()) return;
-    //  @ts-ignore
-    const command = client.commands.get(interaction.commandName);
-
-    if (!command) return;
+client.on('INTERACTION_CREATE', async (interaction: CommandInteraction) => {
+    const isCommand: boolean = interaction.isCommand();
+    if (!isCommand) return;
+    // @ts-ignore
+    const command = await client?.application?.commands.get(interaction.commandName);
 
     try {
-        await command.execute(interaction);
+        await command!.default.execute(interaction);
     } catch (error) {
         console.error(error);
         return interaction.reply({
