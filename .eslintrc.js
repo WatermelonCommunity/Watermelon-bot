46a08a7568c3caa7d91e442b523a1b6a9d76fc46
mrLuisFer <mrluisfeer@gmail.com>
2022-01-09 11:35:03 -0600

    fix indent

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,18 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true
-  },
-  extends: [
-    'standard'
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 13,
-    sourceType: 'module'
-  },
-  plugins: [
-    '@typescript-eslint'
-  ],
-  rules: {
-    semi: ['error', 'always'],
-    indent: 'off'
-  }
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
+    },
+    extends: ['standard', 'prettier'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 13,
+        sourceType: 'module'
+    },
+    plugins: ['@typescript-eslint'],
+    rules: {
+        semi: ['error', 'always'],
+        indent: ['error', 4]
+    }
 };
