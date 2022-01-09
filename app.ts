fa1a3fe989d94e13405c1a4eba9fe9693209f475
mrLuisFer <mrluisfeer@gmail.com>
2022-01-09 11:27:09 -0600

    fix eslint problems for app.ts

--- app.ts
+++ app.ts
@@ -1,49 +1,50 @@
-import { Client, Collection, Intents } from 'discord.js'
-import fs from 'fs'
-import token from './env.json'
+import { Client, Collection, Intents } from 'discord.js';
+import fs from 'fs';
+import token from './env.json';
 
 const client = new Client({
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS],
-})
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS]
+});
 
-client.commands = new Collection()
-const commandFiles: string[] = fs.readdirSync('./src/commands').filter((file) => file.endsWith('.js'))
-const eventFiles: string[] = fs.readdirSync('./src/events').filter((file) => file.endsWith('.js'))
+//  @ts-ignore
+client.commands = new Collection();
+const commandFiles: string[] = fs.readdirSync('./src/commands').filter((file) => file.endsWith('.js'));
+const eventFiles: string[] = fs.readdirSync('./src/events').filter((file) => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-    const command = require(`./src/commands/${file}`)
-    //@ts-ignore
-    client.commands.set(command.default.data.name, command)
+    const command = require(`./src/commands/${file}`);
+    //  @ts-ignore
+    client.commands.set(command.default.data.name, command);
 }
 
 client.on('interactionCreate', async (interaction) => {
-    if (!interaction.isCommand()) return
-    //@ts-ignore
-    const command = client.commands.get(interaction.commandName)
+    if (!interaction.isCommand()) return;
+    //  @ts-ignore
+    const command = client.commands.get(interaction.commandName);
 
-    if (!command) return
+    if (!command) return;
 
     try {
-        await command.execute(interaction)
+        await command.execute(interaction);
     } catch (error) {
-        console.error(error)
+        console.error(error);
         return interaction.reply({
             content: 'There was an error while executing this command!',
             ephemeral: true
-        })
+        });
     }
-})
+});
 
 for (const file of eventFiles) {
-    const event = require(`./src/events/${file}`)
+    const event = require(`./src/events/${file}`);
 
     if (event.once) {
-        client.once(event.name, (...args) => event.execute(...args))
+        client.once(event.name, (...args) => event.execute(...args));
     } else {
-        client.on(event.name, (...args) => event.execute(...args))
+        client.on(event.name, (...args) => event.execute(...args));
     }
 }
 
-client.login(token.token)
+client.login(token.token);
 
-//this does not work
+//  this does not work
