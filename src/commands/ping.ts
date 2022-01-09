601c3df02e6e4c0515288c73d8b59cd6e930f524
mrLuisFer <mrluisfeer@gmail.com>
2022-01-09 13:06:30 -0600

    add interaction type

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,8 +1,9 @@
+import { CommandInteraction } from 'discord.js';
 import { SlashCommandBuilder } from '@discordjs/builders';
 
 export default {
     data: new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!'),
-    async execute(interacion) {
+    async execute(interacion: CommandInteraction) {
         await interacion.reply('Pong!');
     }
 };
