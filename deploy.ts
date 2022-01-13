1a7d9d1a3b2559180e14ad5d45596dbf8a07d943
Zallow's Dev <super.conctacto@gmail.com>
2022-01-13 23:55:54 +0100

    feat: Implement command categorys and set them up into a Collection

--- deploy.ts
+++ deploy.ts
@@ -5,19 +5,31 @@ import fs from 'graceful-fs';
 require('dotenv').config();
 const token: string = process.env.BOT_TOKEN;
 const cmds: Array<any> = [];
-const cmdFiles = fs.readdirSync('./src/commands').filter((f) => f.endsWith('.js'));
+const cmdFolders = fs.readdirSync('./src/commands');
 const rest = new REST({ version: '9' }).setToken(token);
 
 (async () => {
     try {
-        for await (const file of cmdFiles) {
-            const cmd = require(`./src/commands/${file}`);
-            cmds.push(cmd.default.data.toJSON());
-        }
+        // eslint-disable-next-line no-async-promise-executor
+        new Promise(async (resolve, reject) => {
+            try {
+                for await (const folder of cmdFolders) {
+                    const files = fs.readdirSync(`./src/commands/${folder}`).filter((file) => file.endsWith('.js'));
 
-        await rest.put(Routes.applicationGuildCommands('926293354294181959', '926296573959344188'), { body: cmds });
+                    files.forEach((file) => {
+                        const cmd = require(`./src/commands/${folder}/${file}`);
+                        cmds.push(cmd.default.data.toJSON());
+                    });
+                }
 
-        console.log('Slash commands (/) se han cargado al servidor del bot.');
+                resolve('Slash commands (/) se han cargado al servidor del bot');
+            } catch (err) {
+                reject(err);
+            }
+        }).then(async (resp) => {
+            await rest.put(Routes.applicationGuildCommands('926293354294181959', '926296573959344188'), { body: cmds });
+            console.log(resp);
+        });
     } catch (error) {
         console.error(error);
     }
