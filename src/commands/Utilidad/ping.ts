3d9f37e110a3486b4d258702d7e0c52138ecd1aa
BooleanTrue-js <tram24@gmail.com>
2022-01-14 00:31:42 -0400

    Fixed ping command

--- src/commands/Utilidad/ping.ts
+++ src/commands/Utilidad/ping.ts
@@ -1,15 +1,18 @@
 import { CommandInteraction } from 'discord.js/typings/index.js';
 import { SlashCommandBuilder } from '@discordjs/builders';
+import { MessageEmbed } from 'discord.js';
 
 export default {
-    data: new SlashCommandBuilder().setName('ping').setDescription('Devuelve el ping/latencia de el bot actual.'),
+    data: new SlashCommandBuilder().setName('ping').setDescription('Devuelve el ping de el bot actual.'),
     run: async (interaction: CommandInteraction) => {
-        // @ts-ignore
-        const apiPing = Math.round(interaction.client.ws.ping);
+        const pingEmbed = new MessageEmbed()
+            .setColor('#F86761')
+            .setDescription('🏓 Pong')
+            .addField('\u200b', `Ping: ${interaction.client.ws.ping}ms`)
+            .setThumbnail(
+                'https://media.discordapp.net/attachments/888939883471728690/925449636850839622/watermeloncode-logo.png?width=115&height=115'
+            );
 
-        interaction.reply({
-            content: `Ping del bot: ${apiPing}ms\nLatencia de mensajes: ${NaN}ms`, // Soon I will do this
-            ephemeral: false
-        });
+        interaction.reply({ embeds: [pingEmbed] });
     }
 };
